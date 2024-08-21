ccbecc9960d206cad57d21afa37ce2e542f64d49
Sergej Jarosch <sergejjarosch@gmail.com>
2024-08-21 09:33:52 +0200

    Add project files.

---
+++ TableBooking/Models/Bookings.cs
@@ -0,0 +1,14 @@
+﻿namespace TableBooking.Models
+{
+    public class Bookings
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public int PeopleCount { get; set; }
+
+        public string Phone { get; set; }
+    }
+}
